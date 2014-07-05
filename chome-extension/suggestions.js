dd08a567b880cbe05dd4fe990b5adc3e76408eb4
trivago-msanchez <T2eU51,g3>
2014-07-05 17:43:22 +0200

    Updated script files

--- chome-extension/suggestions.js
+++ chome-extension/suggestions.js
@@ -1,23 +1,69 @@
-function getSuggestions()
-{
-    var userRequest = {};
-    userRequest.checkIn = getURLParameter('aDateRange[arr]');
-    userRequest.checkOut = getURLParameter('aDateRange[dep]');
-    var userLocale = $('meta[name=trv-localization]');
-    userRequest.locale = userLocale.attr('data-locale');
+var WTOGO = WTOGO || {};
 
-    $.ajax({
-        url: 'http://192.168.5.120:3000/api/top5',
-        type: 'POST',
-        data: JSON.stringify(userRequest),
-        async: false,
-        success: function(data)
+WTOGO.suggestions = {
+    api: {
+        url: 'localhost',
+        port: ':3000'
+    },
+
+    cities: [],
+
+    container: '',
+
+    getSuggestions: function ()
+    {
+        var self = this;
+        var userRequest = {};
+        userRequest.checkin = getURLParameter(encodeURIComponent('aDateRange[arr]')).replace(/-/g, '');
+        userRequest.checkout = getURLParameter(encodeURIComponent('aDateRange[dep]')).replace(/-/g, '');
+        var userLocale = $('meta[name=trv-localization]');
+        userRequest.locale = userLocale.attr('data-locale');
+
+        $.ajax({
+            url: 'http://' + this.api.url + this.api.port + '/api/top5',
+            type: 'GET',
+            data: userRequest,
+            success: function(data)
+            {
+                var response = JSON.parse(data);
+                self.cities = response.result;
+                self.createSuggestionsContainer();
+                self.addCities();
+            }
+        });
+    },
+
+    createSuggestionsContainer: function ()
+    {
+        var sidebar = $('.sidebar_tabs');
+
+        $('.history').remove('last');
+
+        sidebar.append('<li class="sidebar_element history last" data-state="history"><div>Custom suggestions</div></li>');
+        sidebar.append('<li class="sidebar_display"><div id="js_right_bar_history"><ul class="city_list history wtogo_suggestions">');
+
+        sidebar.append('</ul></div></li>');
+
+        this.container = $('.suggestions');
+    },
+
+    addCities: function ()
+    {
+        var self = this;
+        this.cities.forEach( function (city)
         {
-            var response = JSON.parse(data);
-            cities = response.result;
-        }
-    });
+            var cityBlock = '<li class="path wtogo_suggestion" title="' + city.city_name + ', Double Room" data-path="' + city.path_id + '">';
+            cityBlock += '<div class="info">';
+            cityBlock += '<img width="30" height="30" src="' + city.image_url + '" alt="">';
+            cityBlock += '<div class="js_sidebaritem_city sidebaritem_city_text_wrap">' + city.city_name + ', <strong>' + city.country_name + '</strong></div>';
+            cityBlock += '<strong>Double Room</strong><span class="chronik_item_visited"></span>';
+            cityBlock += '</div></li></ul></div></li>';
+
+            self.container.append(cityBlock);
+        });
+    }
 }
+
 
 function getURLParameter(sParam)
 {
@@ -31,30 +77,4 @@ function getURLParameter(sParam)
             return sParameterName[1];
         }
     }
-}
-
-function addSuggestionsContainer()
-{
-    var sidebar = $('.sidebar_tabs');
-
-    $('.history').remove('last');
-
-    sidebar.append('<li class="sidebar_element history last" data-state="history"><div>Suggestions</div></li>');
-    sidebar.append('<li class="sidebar_display"><div id="js_right_bar_history"><ul class="city_list history suggestions">');
-
-    sidebar.append('</ul></div></li>');
-
-    return $('.suggestions');
-}
-
-function addCity(container, city)
-{
-    var cityBlock = '<li class="path visitable suggestion" title="' + city.city_name + ', Double Room" data-path="' + city.path_id + '" data-roomtype="7">';
-    cityBlock += '<div class="info">';
-    cityBlock += '<img width="30" height="30" src="' + city.image_url + '" alt="">';
-    cityBlock += '<div class="js_sidebaritem_city sidebaritem_city_text_wrap">' + city.city_name + ', <strong>' + city.country_name + '</div>';
-    cityBlock += '<strong>Double Room</strong><span class="chronik_item_visited"></span>';
-    cityBlock += '</div></li></ul></div></li>';
-
-    container.append(cityBlock);
 }
