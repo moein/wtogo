6aa5426bbf2733796f655a06df645e5dd631c0e8
Jose Navarrete <jose.navarrete@trivago.com>
2014-07-05 17:41:55 +0200

    Cleaning comments of life-comparison.js

--- app/life-comparison.js
+++ app/life-comparison.js
@@ -24,7 +24,7 @@ var lifeComparison = {
 
         request(requestedUrl, function(err, response, body) {
             if (200 === response.statusCode && null === err) {
-                var responseText = JSON.stringify(self.filterData(JSON.parse(response.body)));
+                var responseText = JSON.stringify(self.filterData(JSON.parse(response.body), response.statusCode));
                 var response = require('../lib/response');
                 response.send(responseText);
             } else {
@@ -33,73 +33,37 @@ var lifeComparison = {
         });
     },
 
-    filterData: function(responseBody) {
+    filterData: function(responseBody, status) {
         var self = this;
         var _ = require('underscore');
-        return _.filter(responseBody.results['collection1'], function(itemObj) {
+        var data = _.filter(responseBody.results['collection1'], function(itemObj) {
             if (-1 < _.indexOf(self.desiredItems, itemObj.item)){
                 return itemObj;
             }
         });
+
+        return {
+            status: status,
+            differences: data
+        }
     },
 
 }
 
 
 module.exports = function(query){
-    // var sourceCity = query.source_city;
-    // var destinationCity = query.destination_city;
-    //
-    // var request = require('request');
     var config = require('../config/config');
-    //
-    // console.log(config);
-    // var requestedUrl = "https://www.kimonolabs.com/api/dbru15bw?apikey=" + config.kimonoApiKey;
-    // request(requestedUrl, function(err, response, body) {
-    //     console.log(response.statusCode);
-    //     console.log(err);
-    //     if (200 === response.statusCode && null === err) {
-    //         //have fun
-    //     } else {
-    //         return err;
-    //     }
-    // var source = {
-    //     city: query.source_city,
-    //     country: query.source_country
-    // };
-    //
-    // var destination = {
-    //     city: query.destination_city,
-    //     country: query.destination_country
-    // }
+
     var source = {
-        city: 'Madrid',
-        country: 'Spain'
+        city: query.source_city,
+        country: query.source_country
     };
 
     var destination = {
-        city: 'Dusseldorf',
-        country: 'Germany'
-    };
+        city: query.destination_city,
+        country: query.destination_country
+    }
 
+    lifeComparison.getData(source, destination, config);
 
-    // });
-
-    //Do your shit here
-
-    var differences = lifeComparison.getData(source, destination, config);
-
-    var result = {"differences": [
-        {
-            "item": "Beer",
-            "source": "5.4 €",
-            "destination": "6.3 €"
-        },
-        {
-            "item": "Meal, Inexpensive Restaurant",
-            "source": "10.5 €",
-        }
-    ]};
-
-    return {"differences": differences};
 };
