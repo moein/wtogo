e906b629d32411199931899d3d028546bf48e051
trivago-msanchez <T2eU51,g3>
2014-07-05 20:36:49 +0200

    Updated lifecost table

--- chome-extension/comparison.js
+++ chome-extension/comparison.js
@@ -1,5 +1,7 @@
 function displayLifeComparisonInfo(source_city, source_country, destination_city, destination_country)
 {
+    $('#js_pricesearch_transparency').show();
+
     var userRequest = {};
     userRequest.source_city = source_city;
     userRequest.source_country = source_country;
@@ -9,49 +11,73 @@ function displayLifeComparisonInfo(source_city, source_country, destination_city
 
 
     $.ajax({
-        url: 'http://192.168.2.2:3000/api/life-comparison',
+        url: 'http://localhost:3000/api/life-comparison',
         type: 'GET',
         data: userRequest,
         success: function(data)
         {
             var response = JSON.parse(data);
-            addLifeComparisonContent(response.differences);
+            addLifeComparisonContent(response.differences, destination_city);
+            addWeatherComparisonContent(destination_city);
+            addAttractionsContent(destination_city);
+            $('#js_pricesearch_transparency').hide();
         }
     });
 }
 
-function addLifeComparisonContent(differences)
+function addWeatherComparisonContent(destination_city)
 {
-    var lifeComparisonHTML = '<div class ="life-comparison"><br/><br/><br/>';
+    return true;
+}
 
-    lifeComparisonHTML += '<table style="width: 650px;">';
+function addAttractionsContent(destination_city)
+{
+    return true;
+}
 
-    lifeComparisonHTML += '<tr><td colspan="4" style="text-align: center; font-size: 20px;">Life comparison</td></tr>';
-    lifeComparisonHTML += '<tr><td colspan="4"></td></tr>';
+function addLifeComparisonContent(differences, destination_city)
+{
+    var lifeComparisonHTML = '<div class ="life-comparison">';
+    lifeComparisonHTML += '<h2>Your location vs. ' + destination_city + '</h3>';
+    lifeComparisonHTML += '<h3>Life cost</h3>';
 
+    lifeComparisonHTML += '<table>';
+
+    lifeComparisonHTML += '<thead>';
     lifeComparisonHTML += '<tr>';
     lifeComparisonHTML += '<td><b>Criteria</b></td>';
-    lifeComparisonHTML += '<td><b>Destination City</b></td>';
-    lifeComparisonHTML += '<td><b>Current City</b></td>';
+    lifeComparisonHTML += '<td><b>' + destination_city + '</b></td>';
+    lifeComparisonHTML += '<td><b>Your location</b></td>';
     lifeComparisonHTML += '<td><b>Difference</b></td>';
     lifeComparisonHTML += '</tr>';
+    lifeComparisonHTML += '</thead>';
+    lifeComparisonHTML += '<tbody>';
 
     differences.forEach(function(difference_item)
     {
         lifeComparisonHTML += '<tr>';
         lifeComparisonHTML += '<td>' +  difference_item.item + '</td>';
-        lifeComparisonHTML += '<td>' +  difference_item.price_destiny.text + '</td>';
+        lifeComparisonHTML += '<td>' +  difference_item.price_destination.text + '</td>';
         lifeComparisonHTML += '<td>' +  difference_item.price_source.text + '</td>';
-        lifeComparisonHTML += '<td>' +  difference_item.difference + '</td>';
+        lifeComparisonHTML += '<td';
+        if (difference_item.difference.indexOf('-') === 0)
+        {
+            lifeComparisonHTML += ' class="greenlife">';
+        }
+        else
+        {
+            lifeComparisonHTML += ' class="redlife">';
+        }
+        lifeComparisonHTML += difference_item.difference + '</td>';
         lifeComparisonHTML += '</tr>';
-
-
     });
 
+    lifeComparisonHTML += '</tbody>';
     lifeComparisonHTML += '</table>';
     lifeComparisonHTML += '</div>';
 
-    $('.container_itemlist').empty();
+    $('#js_itemlist').empty();
+    $('#js_pagination').empty();
     $('.container_itemlist').append(lifeComparisonHTML);
 
 }
@@ -65,6 +91,3 @@ function displayAttractions()
 {
 
 }
-
-
-displayLifeComparisonInfo('Dusseldorf', 'Germany', 'Palma de Mallorca', 'Spain');
